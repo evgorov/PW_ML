f86ffbac974a396a5f74d714c368c3fb69d7056d
Vladimir Terekhov <zarkzork@gmail.com>
2013-06-04 19:37:06 +0400

    Added unicorn config.

---
+++ unicorn.rb
@@ -0,0 +1,18 @@
+APP_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
+
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
+require 'bundler/setup'
+
+worker_processes 4
+working_directory APP_ROOT
+
+preload_app true
+
+timeout 30
+
+listen "/tmp/unicorn.sock", :backlog => 64
+
+pid APP_ROOT + "/tmp/pids/unicorn.pid"
+
+stderr_path APP_ROOT + "/log/unicorn.stderr.log"
+stdout_path APP_ROOT + "/log/unicorn.stdout.log"
