3c1fa1cf3bcbbe8b8435134a6646620166b3895b
naghtarr <naghtarr@gmail.com>
2013-07-11 15:42:20 +0700

    [przwrd-11] Сменил имя пакета, переименовал активити

--- app/src/main/java/com/ltst/prizeword/navigation/NavigationActivity.java
+++ app/src/main/java/com/ltst/prizeword/navigation/NavigationActivity.java
@@ -1,4 +1,4 @@
-package com.ltst.prizeword;
+package com.ltst.prizeword.navigation;
 
 import android.os.Bundle;
 
@@ -8,10 +8,11 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.ltst.prizeword.R;
 
 import javax.annotation.Nonnull;
 
-public class LauncherActivity extends SherlockActivity
+public class NavigationActivity extends SherlockActivity
 {
     private @Nonnull ListView mDrawerList;
     private @Nonnull ListAdapter mDrawerAdapter;
@@ -20,7 +21,7 @@ public class LauncherActivity extends SherlockActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
+        setContentView(R.layout.activity_navigation);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
         mDrawerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDrawerItems);
         mDrawerList.setAdapter(mDrawerAdapter);
