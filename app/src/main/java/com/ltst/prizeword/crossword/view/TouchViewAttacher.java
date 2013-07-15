f54246521087c37a38767b9f6ef28548325af642
naghtarr <naghtarr@gmail.com>
2013-07-15 20:48:02 +0700

    [przwrd-32] Компонент для рисования кроссвордов, тестовые картинки, простой зум и драг

---
+++ app/src/main/java/com/ltst/prizeword/crossword/view/TouchViewAttacher.java
@@ -0,0 +1,16 @@
+package com.ltst.prizeword.crossword.view;
+
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+/**
+ * Created by root on 7/12/13.
+ */
+public class TouchViewAttacher extends PhotoViewAttacher
+{
+    public TouchViewAttacher(ImageView imageView)
+    {
+        super(imageView);
+    }
+}
