60e4ad0c3fe330c35f20df8bf691c9a70fe21e49
Dmitry <ogndman@gmail.com>
2013-07-16 18:32:03 +0700

    Экран регистрации пользователя (черновой, без datepicker)

--- app/src/main/java/com/ltst/prizeword/login/ForgetPassFragment.java
+++ app/src/main/java/com/ltst/prizeword/login/ForgetPassFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.ltst.prizeword.R;
@@ -17,14 +17,14 @@ import javax.annotation.Nonnull;
  */
 public class ForgetPassFragment extends SherlockFragment implements View.OnClickListener
 {
-    public static final @Nonnull String FRAGMENT_ID = "com.ltst.prizeword.login.LoginFragment";
-    public static final @Nonnull String FRAGMENT_CLASSNAME = LoginFragment.class.getName();
+    public static final @Nonnull String FRAGMENT_ID = "com.ltst.prizeword.login.ForgetPassFragment";
+    public static final @Nonnull String FRAGMENT_CLASSNAME = ForgetPassFragment.class.getName();
 
     @SuppressWarnings("unchecked")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View v = inflater.inflate(R.layout.login_fragment_layout, container, false);
+        View v = inflater.inflate(R.layout.forgetpass_fragment_layout, container, false);
         return v;
     }
 
