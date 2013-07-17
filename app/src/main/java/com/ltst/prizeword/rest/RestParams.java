41acbdaceaf2d475a551bf02ab8df1f727a183df
naghtarr <naghtarr@gmail.com>
2013-07-17 11:33:07 +0700

    [przwrd-33] Класс хранения REST параметров

---
+++ app/src/main/java/com/ltst/prizeword/rest/RestParams.java
@@ -0,0 +1,10 @@
+package com.ltst.prizeword.rest;
+
+import javax.annotation.Nonnull;
+
+public class RestParams
+{
+    public static final @Nonnull String API_URL = "http://api.prize-word.com";
+    public static final @Nonnull String PROVIDER_LOGIN = API_URL + "/%s/login";
+    public static final @Nonnull String SIGN_UP = API_URL + "/signup";
+}
