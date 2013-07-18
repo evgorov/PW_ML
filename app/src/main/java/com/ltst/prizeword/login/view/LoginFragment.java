46a0fdfb34a660bb8b6c1d284b5602ad0c50ad6b
Dmitry <ogndman@gmail.com>
2013-07-18 16:49:11 +0700

    Экран логина и авторизации

--- app/src/main/java/com/ltst/prizeword/login/view/LoginFragment.java
+++ app/src/main/java/com/ltst/prizeword/login/view/LoginFragment.java
@@ -21,7 +21,6 @@ public class LoginFragment extends SherlockFragment implements OnClickListener
     public static final @Nonnull String FRAGMENT_ID = "com.ltst.prizeword.login.LoginFragment";
     public static final @Nonnull String FRAGMENT_CLASSNAME = LoginFragment.class.getName();
 
-    private @Nonnull Button mAuthorizationButton;
     private @Nonnull IFragmentsHolderActivity mFragmentHolder;
 
     @SuppressWarnings("unchecked")
@@ -29,14 +28,13 @@ public class LoginFragment extends SherlockFragment implements OnClickListener
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.login_fragment_layout, container, false);
-        mAuthorizationButton = (Button) v.findViewById(R.id.enter_to_authorization_btn);
+
         return v;
     }
 
     public void onActivityCreated(Bundle saveInstanceState)
     {
         mFragmentHolder = (IFragmentsHolderActivity) getActivity();
-        mAuthorizationButton.setOnClickListener(this);
         super.onActivityCreated(saveInstanceState);
     }
 
@@ -45,20 +43,7 @@ public class LoginFragment extends SherlockFragment implements OnClickListener
     {
         switch (v.getId())
         {
-            case R.id.enter_with_fb_btn:
-                //Для facebook
-                break;
-            case R.id.enter_with_vk_btn:
-                //Для вконтакте
-                break;
-            case R.id.enter_to_authorization_btn:
-                mFragmentHolder.selectNavigationFragmentByClassname(AuthorizationFragment.FRAGMENT_CLASSNAME);
-                break;
-            case R.id.enter_to_registration_btn:
-                mFragmentHolder.selectNavigationFragmentByClassname(RegisterFragment.FRAGMENT_CLASSNAME);
-                break;
-            default:
-                break;
+
         }
     }
 }
