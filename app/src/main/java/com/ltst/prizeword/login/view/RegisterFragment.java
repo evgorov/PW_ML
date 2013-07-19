2bd9ef99ecdc429fc95ebc8780f4c00a373a0df0
Dmitry <ogndman@gmail.com>
2013-07-19 20:27:33 +0700

    date picker и экраны по центру

--- app/src/main/java/com/ltst/prizeword/login/view/RegisterFragment.java
+++ app/src/main/java/com/ltst/prizeword/login/view/RegisterFragment.java
@@ -7,15 +7,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.FrameLayout;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.ltst.prizeword.R;
 import com.ltst.prizeword.navigation.IFragmentsHolderActivity;
 import com.ltst.prizeword.navigation.INavigationBackPress;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.annotation.Nonnull;
 
-public class RegisterFragment extends SherlockFragment implements INavigationBackPress
+import static android.view.View.VISIBLE;
+
+public class RegisterFragment extends SherlockFragment implements View.OnClickListener, INavigationBackPress
 {
     public static final @Nonnull
     String FRAGMENT_ID = "com.ltst.prizeword.login.view.RegisterFragment";
@@ -23,9 +32,14 @@ public class RegisterFragment extends SherlockFragment implements INavigationBac
 
     private @Nonnull Context mContext;
 
+    private @Nonnull Button mRegisterDateButton;
     private @Nonnull Button mNavBackButton;
+    private @Nonnull Button mRegisterSetDateButton;
+    private @Nonnull FrameLayout mDatePickerFrame;
+    private @Nonnull DatePicker mDatePicker;
     private @Nonnull IFragmentsHolderActivity mFragmentHolder;
-
+    SimpleDateFormat format;
+    Date date;
     @Override
     public void onAttach(Activity activity)
     {
@@ -40,6 +54,10 @@ public class RegisterFragment extends SherlockFragment implements INavigationBac
     {
         View v = inflater.inflate(R.layout.register_fragment_layout, container, false);
         mNavBackButton = (Button) v.findViewById(R.id.registration_nav_back_button);
+        mRegisterDateButton = (Button) v.findViewById(R.id.register_date_born_btn);
+        mRegisterSetDateButton = (Button) v.findViewById(R.id.register_set_date_btn);
+        mDatePickerFrame = (FrameLayout) v.findViewById(R.id.date_picker_frame);
+        mDatePicker = (DatePicker)  v.findViewById(R.id.datePicker1);
         mNavBackButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -48,6 +66,12 @@ public class RegisterFragment extends SherlockFragment implements INavigationBac
                 onBackKeyPress();
             }
         });
+        mRegisterDateButton.setOnClickListener(this);
+        mRegisterSetDateButton.setOnClickListener(this);
+        String pattern = "yyyy-MM-dd";
+        format = new SimpleDateFormat(pattern);
+
+
         return v;
     }
 
@@ -55,5 +79,25 @@ public class RegisterFragment extends SherlockFragment implements INavigationBac
     public void onBackKeyPress()
     {
         mFragmentHolder.selectNavigationFragmentByClassname(LoginFragment.FRAGMENT_CLASSNAME);
+    }
+
+    @Override public void onClick(View v)
+    {
+        switch (v.getId()){
+            case R.id.register_date_born_btn:
+                mDatePickerFrame.setVisibility(VISIBLE);
+                break;
+            case R.id.register_set_date_btn:
+                mDatePickerFrame.setVisibility(View.GONE);
+                try
+                {
+                    date=format.parse(mDatePicker.getYear()+"-"+mDatePicker.getMonth()+"-"+ mDatePicker.getDayOfMonth());
+                } catch (ParseException e)
+                {
+                    e.printStackTrace();
+                }
+                mRegisterDateButton.setText(format.format(date));
+                break;
+        }
     }
 }
