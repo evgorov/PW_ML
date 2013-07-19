c238d58544d70c5dd74bb2b111718d46cfe9d2e2
naghtarr <naghtarr@gmail.com>
2013-07-19 13:20:18 +0700

    Пофиксил экран регистрации

--- app/src/main/java/com/ltst/prizeword/login/view/RegisterFragment.java
+++ app/src/main/java/com/ltst/prizeword/login/view/RegisterFragment.java
@@ -12,31 +12,28 @@ import com.ltst.prizeword.R;
 
 import javax.annotation.Nonnull;
 
-public class RegisterFragment extends SherlockFragment implements View.OnClickListener
+public class RegisterFragment extends SherlockFragment
 {
-    public static final @Nonnull String FRAGMENT_ID = "com.ltst.prizeword.login.view.RegisterFragment";
+    public static final @Nonnull
+    String FRAGMENT_ID = "com.ltst.prizeword.login.view.RegisterFragment";
     public static final @Nonnull String FRAGMENT_CLASSNAME = RegisterFragment.class.getName();
 
     private @Nonnull Context mContext;
 
 
     @Override
-    public void onAttach(Activity activity) {
+    public void onAttach(Activity activity)
+    {
         super.onAttach(activity);
         mContext = (Context) activity;
     }
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
 
-    public View OnCreateVeiw(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.register_fragment_layout, container, false);
         return v;
     }
 
-    @Override public void onClick(View view)
-    {
-
-    }
 }
