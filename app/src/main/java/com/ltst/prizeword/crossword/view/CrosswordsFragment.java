5ddf37776025955f72210e22ebda603406ade048
Александр Мирко <cosic333@yahoo.com>
2013-08-09 20:52:46 +0700

    [przwrd-116] готовая верстка на показ

--- app/src/main/java/com/ltst/prizeword/crossword/view/CrosswordsFragment.java
+++ app/src/main/java/com/ltst/prizeword/crossword/view/CrosswordsFragment.java
@@ -5,11 +5,16 @@ import com.ltst.prizeword.R;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.ltst.prizeword.app.IBcConnectorOwner;
@@ -34,11 +39,18 @@ public class CrosswordsFragment extends SherlockFragment
     public static final @Nonnull String FRAGMENT_CLASSNAME = CrosswordsFragment.class.getName();
 
     private @Nonnull Context mContext;
-    private @Nonnull Button mCrossWordButton;
+//    private @Nonnull Button mCrossWordButton;
+
+    private static final int BADGE_ID = 1;
 
     private @Nonnull IBcConnector mBcConnector;
     private @Nonnull String mSessionKey;
     private @Nonnull IPuzzleSetModel mPuzzleSetModel;
+
+    private @Nonnull LinearLayout mViewCurrentContainer;
+    private @Nonnull LinearLayout mViewArchiveContainer;
+
+    private @Nonnull Button mMenuBackButton;
 
     // ==== Livecycle =================================
 
@@ -55,7 +67,39 @@ public class CrosswordsFragment extends SherlockFragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.crossword_fragment_layout, container, false);
-        mCrossWordButton = (Button) v.findViewById(R.id.view_crossword);
+        mViewCurrentContainer = (LinearLayout) v.findViewById(R.id.crossword_fragment_current_container);
+        mViewArchiveContainer = (LinearLayout) v.findViewById(R.id.crossword_fragment_archive_container);
+        mMenuBackButton = (Button) v.findViewById(R.id.crossword_fragment_header_menu_btn);
+        mMenuBackButton.setOnClickListener(this);
+
+        LinearLayout viewCurrentBrilliant = (LinearLayout) inflater.inflate(R.layout.crossword_current_brilliant, null, false);
+        LinearLayout viewCurrentFree = (LinearLayout) inflater.inflate(R.layout.crossword_current_free, null, false);
+
+        LinearLayout viewArchiveGold = (LinearLayout) inflater.inflate(R.layout.crossword_archive_gold, null, false);
+        LinearLayout viewArchiveSilver = (LinearLayout) inflater.inflate(R.layout.crossword_archive_silver, null, false);
+
+        ((LinearLayout) viewCurrentFree.findViewById(R.id.crossword_current_free_buy_panel)).setVisibility(View.GONE);
+        ((LinearLayout) viewArchiveGold.findViewById(R.id.crossword_archive_gold_splitter)).setVisibility(View.GONE);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.crossword_number_free_1);
+        ImageView iv = new ImageView(mContext);
+        iv.setImageBitmap(bitmap);
+//        bitmap.recycle();
+
+        View badgeCurrentFree1 = inflater.inflate(R.layout.crossword_badge_free_unresolved, null, false);
+        badgeCurrentFree1.setId(BADGE_ID);
+        badgeCurrentFree1.setOnClickListener(this);
+
+        ((LinearLayout)badgeCurrentFree1.findViewById(R.id.crossword_badge_unresolved_free_number_container)).addView(iv);
+
+        viewCurrentFree.addView(badgeCurrentFree1);
+
+        mViewCurrentContainer.addView(viewCurrentBrilliant);
+        mViewCurrentContainer.addView(viewCurrentFree);
+
+        mViewArchiveContainer.addView(viewArchiveGold);
+        mViewArchiveContainer.addView(viewArchiveSilver);
+
         return v;
     }
 
@@ -72,7 +116,7 @@ public class CrosswordsFragment extends SherlockFragment
     @Override
     public void onActivityCreated(Bundle savedInstanceState)
     {
-        mCrossWordButton.setOnClickListener(this);
+//        mCrossWordButton.setOnClickListener(this);
 
         super.onActivityCreated(savedInstanceState);
     }
@@ -84,8 +128,13 @@ public class CrosswordsFragment extends SherlockFragment
     {
         switch (view.getId())
         {
-            case R.id.view_crossword:
+            case BADGE_ID:
+//            case R.id.view_crossword:
                 launchCrosswordActivity();
+                break;
+
+            case R.id.crossword_fragment_header_menu_btn:
+
                 break;
 
             default:
