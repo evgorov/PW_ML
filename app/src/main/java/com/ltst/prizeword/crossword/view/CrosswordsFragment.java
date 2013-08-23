b881ed82e86f6aab64aee0046fe05fa474cb8235
Dmitry <ogndman@gmail.com>
2013-08-23 13:50:20 +0700

    [przwrd-199] сделал новостную ленту на экане сканвордов

--- app/src/main/java/com/ltst/prizeword/crossword/view/CrosswordsFragment.java
+++ app/src/main/java/com/ltst/prizeword/crossword/view/CrosswordsFragment.java
@@ -6,10 +6,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
@@ -20,6 +25,8 @@ import com.ltst.prizeword.crossword.model.Puzzle;
 import com.ltst.prizeword.crossword.model.PuzzleSet;
 import com.ltst.prizeword.crossword.model.PuzzleSetModel;
 import com.ltst.prizeword.navigation.INavigationDrawerHolder;
+import com.ltst.prizeword.swipe.ITouchInterface;
+import com.ltst.prizeword.swipe.TouchDetector;
 
 import org.omich.velo.bcops.client.IBcConnector;
 import org.omich.velo.handlers.IListenerInt;
@@ -32,8 +39,8 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class CrosswordsFragment extends SherlockFragment
-                                implements View.OnClickListener,
-                                ICrosswordFragment
+        implements View.OnClickListener,
+        ICrosswordFragment, ITouchInterface
 {
     public static final @Nonnull String FRAGMENT_ID = "com.ltst.prizeword.crossword.mRootView.CrosswordsFragment";
     public static final @Nonnull String FRAGMENT_CLASSNAME = CrosswordsFragment.class.getName();
@@ -50,6 +57,15 @@ public class CrosswordsFragment extends SherlockFragment
     private @Nonnull TextView mHintsCountView;
     private @Nonnull Button mMenuBackButton;
     private @Nonnull CrosswordFragmentHolder mCrosswordFragmentHolder;
+    private @Nonnull int[] mStringsMassive;
+    private @Nonnull RelativeLayout mNewsLayout;
+    private @Nonnull GestureDetector mGestureDetector;
+    private @Nonnull LinearLayout mNewsIndicatorLayout;
+    private @Nonnull ImageView mSimpleImage;
+    private @Nonnull TextView mNewsSimpleText;
+    private @Nonnull ImageView mNewsCloseBtn;
+    private int mIndicatorPosition;
+
 //    private @Nonnull IOnePuzzleModel mPuzzleModel;
 
     // ==== Livecycle =================================
@@ -58,7 +74,7 @@ public class CrosswordsFragment extends SherlockFragment
     public void onAttach(Activity activity)
     {
         mContext = (Context) activity;
-        mBcConnector = ((IBcConnectorOwner)activity).getBcConnector();
+        mBcConnector = ((IBcConnectorOwner) activity).getBcConnector();
         mINavigationDrawerHolder = (INavigationDrawerHolder) activity;
 
         super.onAttach(activity);
@@ -76,6 +92,12 @@ public class CrosswordsFragment extends SherlockFragment
 
         mHintsCountView = (TextView) v.findViewById(R.id.crossword_fragment_current_rest_count);
 
+        mStringsMassive = new int[]{R.string.news1, R.string.news2, R.string.news3, R.string.news4, R.string.news5};
+        mNewsLayout = (RelativeLayout) v.findViewById(R.id.news_layout);
+        mGestureDetector = new GestureDetector(mContext, new TouchDetector(this));
+        mNewsIndicatorLayout = (LinearLayout) v.findViewById(R.id.news_indicator_layout);
+        mNewsSimpleText = (TextView) v.findViewById(R.id.news_simple_text);
+        mNewsCloseBtn = (ImageView) v.findViewById(R.id.news_close_btn);
         mRoot = v;
         return v;
     }
@@ -91,6 +113,25 @@ public class CrosswordsFragment extends SherlockFragment
         mPuzzleSetModel.updateTotalDataByDb(updateSetsFromDBHandler);
 //        mPuzzleSetModel.updateDataByDb(updateSetsFromDBHandler);
 //        mPuzzleSetModel.updateTotalDataByInternet(updateSetsFromServerHandler);
+
+        LinearLayout.LayoutParams params;
+        for (int i = 0; i < mStringsMassive.length; i++)
+        {
+            mSimpleImage = new ImageView(mContext);
+            mSimpleImage.setImageResource(R.drawable.puzzles_news_page);
+            params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            if (i != 0)
+                params.setMargins(4, 0, 0, 0);
+            mSimpleImage.setLayoutParams(params);
+            mSimpleImage.setId(i);
+
+            mNewsIndicatorLayout.addView(mSimpleImage);
+        }
+        mIndicatorPosition = 0;
+        mSimpleImage = (ImageView) mRoot.findViewById(mIndicatorPosition);
+        mSimpleImage.setImageResource(R.drawable.puzzles_news_page_current);
+        mNewsSimpleText.setText(mStringsMassive[mIndicatorPosition]);
+
         super.onResume();
     }
 
@@ -98,6 +139,14 @@ public class CrosswordsFragment extends SherlockFragment
     public void onActivityCreated(Bundle savedInstanceState)
     {
 //        mCrossWordButton.setOnClickListener(this);
+        mNewsLayout.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                return mGestureDetector.onTouchEvent(motionEvent);
+            }
+        });
+        mNewsCloseBtn.setOnClickListener(this);
 
         super.onActivityCreated(savedInstanceState);
     }
@@ -110,12 +159,13 @@ public class CrosswordsFragment extends SherlockFragment
         switch (view.getId())
         {
             case R.id.crossword_fragment_header_menu_btn:
-                if(mINavigationDrawerHolder.isLockDrawerOpen())
+                if (mINavigationDrawerHolder.isLockDrawerOpen())
                     mINavigationDrawerHolder.lockDrawerClosed();
                 else
                     mINavigationDrawerHolder.lockDrawerOpened();
                 break;
-
+            case R.id.news_close_btn:
+                mNewsLayout.setVisibility(View.GONE);
             default:
                 break;
         }
@@ -129,7 +179,7 @@ public class CrosswordsFragment extends SherlockFragment
         @Nullable PuzzleSet freeSet = null;
         for (PuzzleSet set : sets)
         {
-            if(set.type.equals(PuzzleSetModel.FREE))
+            if (set.type.equals(PuzzleSetModel.FREE))
             {
                 freeSet = set;
                 break;
@@ -142,14 +192,15 @@ public class CrosswordsFragment extends SherlockFragment
         }
     }
 
-    private void createCrosswordPanel(){
+    private void createCrosswordPanel()
+    {
         @Nonnull List<PuzzleSet> sets = mPuzzleSetModel.getPuzzleSets();
         @Nonnull HashMap<String, List<Puzzle>> mapPuzzles = mPuzzleSetModel.getPuzzlesSet();
         for (PuzzleSet set : sets)
         {
             mCrosswordFragmentHolder.addPanel(set);
             @Nonnull List<Puzzle> puzzles = mapPuzzles.get(set.serverId);
-            for(@Nonnull Puzzle puzzle : puzzles)
+            for (@Nonnull Puzzle puzzle : puzzles)
             {
                 mCrosswordFragmentHolder.addBadge(puzzle);
             }
@@ -189,14 +240,34 @@ public class CrosswordsFragment extends SherlockFragment
     };
 
     @Override
-    public void buyCrosswordSet(String crosswordSetServerId) {
+    public void buyCrosswordSet(String crosswordSetServerId)
+    {
 
     }
 
     @Override
-    public void choiceCrossword() {
+    public void choiceCrossword()
+    {
         launchCrosswordActivity();
     }
 
 
+    @Override public void notifySwipe(SwipeMethod swipe)
+    {
+        mSimpleImage = (ImageView) mRoot.findViewById(mIndicatorPosition);
+        mSimpleImage.setImageResource(R.drawable.puzzles_news_page);
+        if (swipe.equals(SwipeMethod.SWIPE_RIGHT))
+        {
+            if (mIndicatorPosition > 0)
+                mIndicatorPosition--;
+        } else if (swipe.equals(SwipeMethod.SWIPE_LEFT))
+        {
+            if (mIndicatorPosition < mStringsMassive.length-1)
+                mIndicatorPosition++;
+        }
+        mSimpleImage = (ImageView) mRoot.findViewById(mIndicatorPosition);
+        mSimpleImage.setImageResource(R.drawable.puzzles_news_page_current);
+        mNewsSimpleText.setText(mStringsMassive[mIndicatorPosition]);
+
+    }
 }
