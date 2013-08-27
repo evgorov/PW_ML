a5082f35814d7dd4ff3b7ce98546d1ffb188696f
Александр Мирко <cosic333@yahoo.com>
2013-08-27 13:18:52 +0700

    [przwrd-206] Очистка БД при разлогировании

---
+++ app/src/main/java/com/ltst/prizeword/crossword/model/ClearDataBase.java
@@ -0,0 +1,45 @@
+package com.ltst.prizeword.crossword.model;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.ltst.prizeword.R;
+import com.ltst.prizeword.db.DbService;
+
+import org.omich.velo.bcops.BcTaskHelper;
+import org.omich.velo.cast.NonnullableCasts;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Created by cosic on 27.08.13.
+ */
+public class ClearDataBase implements DbService.IDbTask {
+
+    static final public @Nonnull Intent createIntent()
+    {
+        Intent intent = new Intent();
+        return intent;
+    }
+
+
+    @Nullable
+    @Override
+    public Bundle execute(@Nonnull DbService.DbTaskEnv env) {
+
+        if(!BcTaskHelper.isNetworkAvailable(env.context))
+        {
+            env.bcToaster.showToast(
+                    NonnullableCasts.getStringOrEmpty(
+                            env.context.getString(R.string.msg_no_internet)));
+        }
+        else
+        {
+            env.dbw.clearDb();
+        }
+
+        return null;
+    }
+
+}
--- app/src/main/java/com/ltst/prizeword/login/model/IUserDataModel.java
+++ app/src/main/java/com/ltst/prizeword/login/model/IUserDataModel.java
@@ -16,4 +16,5 @@ public interface IUserDataModel {
     void resetUserName(final String userName, @Nonnull IListenerVoid handler);
     void loadProvidersFromDB(long user_id, @Nonnull IListenerVoid handler);
     void mergeAccounts(final @Nonnull String sessionKey1, final @Nonnull String sessionKey2, @Nonnull IListenerVoid handler);
+    void clearDataBase(@Nonnull IListenerVoid handler);
 }
