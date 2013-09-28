71dd412c54f3a00ab5c62965ba6ae7dafa29cfdb
Dmitry <suade@mail.ru>
2013-09-28 16:01:41 +0700

    Поправил отправку инвайта ВК, сделал Хендлер с прогрессбаром

--- app/src/main/java/com/ltst/prizeword/invitefriends/view/InviteFragmentAdapter.java
+++ app/src/main/java/com/ltst/prizeword/invitefriends/view/InviteFragmentAdapter.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.ltst.prizeword.R;
@@ -20,16 +21,14 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 
-public class InviteFragmentAdapter extends SlowSourceAdapter<InviteFragmentAdapter.ViewHolder, InviteFriendsData, Bitmap>
-{
+public class InviteFragmentAdapter extends SlowSourceAdapter<InviteFragmentAdapter.ViewHolder, InviteFriendsData, Bitmap> {
     private final @Nonnull IInviteFriendsDataModel mModel;
     private @Nullable IListenerVoid mRefreshHandler;
     private @Nonnull Context mContext;
     private boolean mFbSwitch;
     private boolean mVkSwitch;
 
-    public InviteFragmentAdapter(@Nonnull Context context, @Nonnull IInviteFriendsDataModel model, boolean fbSwitch, boolean vkSwitch)
-    {
+    public InviteFragmentAdapter(@Nonnull Context context, @Nonnull IInviteFriendsDataModel model, boolean fbSwitch, boolean vkSwitch) {
         super(context, model.getSource());
         mContext = context;
         mModel = model;
@@ -38,12 +37,9 @@ public class InviteFragmentAdapter extends SlowSourceAdapter<InviteFragmentAdapt
         updateByInternet();
     }
 
-    public void updateByInternet()
-    {
-        mModel.updateDataByInternet(new IListenerVoid()
-        {
-            public void handle()
-            {
+    public void updateByInternet() {
+        mModel.updateDataByInternet(new IListenerVoid() {
+            public void handle() {
                 if (mRefreshHandler != null)
                     mRefreshHandler.handle();
                 setSlowSource(mModel.getSource());
@@ -51,8 +47,7 @@ public class InviteFragmentAdapter extends SlowSourceAdapter<InviteFragmentAdapt
         });
     }
 
-    public void setRefreshHandler(@Nonnull IListenerVoid handler)
-    {
+    public void setRefreshHandler(@Nonnull IListenerVoid handler) {
         this.mRefreshHandler = handler;
     }
 
@@ -60,41 +55,39 @@ public class InviteFragmentAdapter extends SlowSourceAdapter<InviteFragmentAdapt
 
     @Override
     protected
-    @Nonnull ViewHolder createViewHolderOfView(@Nonnull View view)
-    {
+    @Nonnull
+    ViewHolder createViewHolderOfView(@Nonnull View view) {
         TextView nameView = (TextView) view.findViewById(R.id.invite_item_name_textview);
         TextView surnameView = (TextView) view.findViewById(R.id.invite_item_surname_textview);
         ImageView imageView = (ImageView) view.findViewById(R.id.invite_item_ava);
         Button inviteBtn = (Button) view.findViewById(R.id.invite_add_btn);
         View layout = view.findViewById(R.id.invite_item_layout);
-        if (nameView == null || surnameView == null || imageView == null || inviteBtn == null || layout == null)
-        {
+        ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.invite_item_progressBar);
+        if (nameView == null || surnameView == null || imageView == null || inviteBtn == null || layout == null || progressBar == null) {
             throw new NullPointerException("Elements nameView, surnameView or imageView was null, but they must not be"); //$NON-NLS-1$
         }
-        return new ViewHolder(nameView, surnameView, imageView, inviteBtn, layout);
+        return new ViewHolder(nameView, surnameView, imageView, inviteBtn, layout, progressBar);
     }
 
 
     @Override
-    protected void appendQuickDataToView(@Nonnull ViewHolder viewHolder, @Nonnull InviteFriendsData quick, @Deprecated @Nonnull View view, @Deprecated int position)
-    {
+    protected void appendQuickDataToView(@Nonnull ViewHolder viewHolder, @Nonnull InviteFriendsData quick, @Deprecated @Nonnull View view, @Deprecated int position) {
+        final ProgressBar prBar = viewHolder.progressBar;
+        final Button btn = viewHolder.inviteBtn;
         final String id = quick.id;
         final String provider = quick.providerName;
-        if (provider.equals(InviteFriendsData.NO_PROVIDER))
-        {
+        if (provider.equals(InviteFriendsData.NO_PROVIDER)) {
             if (mVkSwitch)
                 viewHolder.layout.setBackgroundResource(R.drawable.invite_fb_header);
             else if (!mVkSwitch)
                 viewHolder.layout.setBackgroundResource(R.drawable.invite_fb_header_only);
-            else if(!mFbSwitch)
+            else if (!mFbSwitch)
                 viewHolder.layout.setVisibility(View.GONE);
             viewHolder.imageView.setVisibility(View.GONE);
             viewHolder.inviteBtn.setVisibility(View.GONE);
             viewHolder.nameView.setVisibility(View.GONE);
             viewHolder.surnameView.setVisibility(View.GONE);
-        }
-        else
-        {
+        } else {
             viewHolder.layout.setBackgroundResource(R.drawable.invite_item_bg);
             viewHolder.imageView.setVisibility(View.VISIBLE);
             viewHolder.inviteBtn.setVisibility(View.VISIBLE);
@@ -104,66 +97,67 @@ public class InviteFragmentAdapter extends SlowSourceAdapter<InviteFragmentAdapt
 
         viewHolder.nameView.setText(quick.firstName);
         viewHolder.surnameView.setText(quick.lastName);
-        if (quick.status.equals("already_registered"))
-        {
+        if (quick.status.equals("already_registered")) {
             viewHolder.inviteBtn.setEnabled(false);
-            viewHolder.inviteBtn.setOnClickListener(new View.OnClickListener()
-            {
+
+        } else if (quick.status.equals("uninvited") || quick.status.equals("invite_sent")) {
+            viewHolder.inviteBtn.setEnabled(true);
+            viewHolder.inviteBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v)
-                {
+                public void onClick(View v) {
                     SoundsWork.interfaceBtnMusic(mContext);
-                    switch (v.getId())
-                    {
+                    switch (v.getId()) {
                         case R.id.invite_add_btn:
-                            invite(id,provider,null);
+                            prBar.setVisibility(View.VISIBLE);
+                            invite(id, provider, new IListenerVoid() {
+                                @Override
+                                public void handle() {
+                                    prBar.setVisibility(View.GONE);
+                                    btn.setEnabled(false);
+                                }
+                            });
                             break;
                     }
                 }
             });
-        } else if (quick.status.equals("uninvited") || quick.status.equals("invite_sent"))
-        {
-            viewHolder.inviteBtn.setEnabled(true);
         }
         viewHolder.imageView.setImageDrawable(mContext.getResources().getDrawable(R.drawable.rating_userpic_none));
     }
 
     @Override
-    protected void appendSlowDataToView(@Nonnull ViewHolder viewHolder, @Nonnull Bitmap slow, @Deprecated @Nonnull View view, @Deprecated int position)
-    {
+    protected void appendSlowDataToView(@Nonnull ViewHolder viewHolder, @Nonnull Bitmap slow, @Deprecated @Nonnull View view, @Deprecated int position) {
         viewHolder.imageView.setImageBitmap(slow);
     }
 
     @Override
-    protected int getItemViewResId()
-    {
+    protected int getItemViewResId() {
         return R.layout.invite_simple_item;
     }
 
-    public void invite(@Nonnull String id,@Nonnull String provider, @Nullable IListenerVoid handler)
-    {
+    public void invite(@Nonnull String id, @Nonnull String provider, @Nullable IListenerVoid handler) {
         mModel.sendInviteFriends(id, provider, handler);
     }
 
 
     //=========================================================================
-    protected static class ViewHolder
-    {
+    protected static class ViewHolder {
         final @Nonnull TextView nameView;
         final @Nonnull TextView surnameView;
         final @Nonnull ImageView imageView;
         final @Nonnull Button inviteBtn;
         final @Nonnull View layout;
+        final @Nonnull ProgressBar progressBar;
 
         private ViewHolder(@Nonnull TextView nameView, @Nonnull TextView surnameView,
-                           @Nonnull ImageView imageView, @Nonnull Button inviteBtn, @Nonnull View layout)
-        {
+                           @Nonnull ImageView imageView, @Nonnull Button inviteBtn, @Nonnull View layout, @Nonnull ProgressBar progressBar) {
             this.nameView = nameView;
             this.surnameView = surnameView;
             this.imageView = imageView;
             this.inviteBtn = inviteBtn;
             this.layout = layout;
+            this.progressBar = progressBar;
         }
     }
+
 
 }
