99fcdaa8d15ffa55a979ec644429bec324934e5c
Pavel Skorynin <pavel.skorynin@gmail.com>
2013-10-17 16:06:07 +0600

    ~ fix TapJoy integration + Flurry integration

--- PrizeWord/AppDelegate.m
+++ PrizeWord/AppDelegate.m
@@ -67,9 +67,9 @@ static NSRecursiveLock * dbLock = nil;
     _isIPad = ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad);
     [Crashlytics startWithAPIKey:@"41fbbeb2af9ef97717a85b18418a3cb2055ebcb9"];
     
-    [Tapjoy requestTapjoyConnect:@"b77b17c3-ccd3-4aba-979d-5307edce0a8b" secretKey:@"ZspEG1Io8dB6ETzFNnbc"];
+    [Tapjoy requestTapjoyConnect:@"b77b17c3-ccd3-4aba-979d-5307edce0a8b" secretKey:@"ZspEG1Io8dB6ETzFNnbc" options:@{TJC_OPTION_ENABLE_LOGGING : @(YES)}];
     
-//    [Flurry startSession:@""];
+    [Flurry startSession:@"DBWS85GPDMJWKCDDXH39"];
     
     [application setStatusBarHidden:YES];
     _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
